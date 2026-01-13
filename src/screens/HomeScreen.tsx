50b774e06b215eb3aee17d309f1529624b40eb9c
Edgar <edgargarciagonzalez@elpuig.xeill.net>
2026-01-13 16:34:53 +0100

    prueba

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, padding: 24, backgroundColor: "black" }}>
+      <Text style={{ fontSize: 22, fontWeight: "600" }}>Inicio</Text>
+    </View>
+  );
+}
